6caabc640ae2330b7e4e54438e88aaad958097b0
Jordan Geurtsen <j.geurtsen1@student.han.nl>
2023-09-20 20:49:33 +0200

    Eliminated ASCII values 129, 141, 143, 144 and 157 from the loop, because these ASCII values should be undifined

--- prototypes/createDictionary.hs
+++ prototypes/createDictionary.hs
@@ -4,6 +4,7 @@ initDir :: Int -> [(Int, String)]
 initDir x
     | x < 0     = []
     | x > 255   = []
+    | x `elem` [129, 141, 143, 144, 157] = initDir (x + 1)
     | otherwise = (x, [chr x]) : initDir (x + 1)
 
 main :: IO ()
