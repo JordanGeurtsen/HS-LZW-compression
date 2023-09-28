5526dafa138edf803002e038855fb4e6fcbbf3e6
Jordan Geurtsen <j.geurtsen1@student.han.nl>
2023-09-28 12:47:56 +0200

    Compression ratio was flipped

--- Utils.hs
+++ Utils.hs
@@ -6,6 +6,6 @@ getCompressionInfo :: String -> String -> IO [Float]
 getCompressionInfo inputFileName outputFileName = do
   inputFileSize <- getFileSize inputFileName
   outputFileSize <- getFileSize outputFileName
-  let ratio = (fromIntegral inputFileSize) / (fromIntegral outputFileSize)
-  let percentSaved = (ratio * 100) - 100
+  let ratio = (fromIntegral outputFileSize) / (fromIntegral inputFileSize)
+  let percentSaved = 100 - (ratio * 100)
   return [fromIntegral inputFileSize, fromIntegral outputFileSize, ratio, percentSaved]
