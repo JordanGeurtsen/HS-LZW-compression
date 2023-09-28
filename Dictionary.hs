3d065c76f459e822afbb03fd4186f2889b74dbb7
Jordan Geurtsen <j.geurtsen1@student.han.nl>
2023-09-28 11:37:09 +0200

    Put the basic functions, init, getFrom, contains, putIn to Dictionary module, to prevent redundancy later on in the compressor module

--- Dictionary.hs
+++ Dictionary.hs
@@ -1,3 +1,18 @@
 module Dictionary where
 
 import qualified Data.Map as Map
+import System.Directory
+
+type Directory = Map.Map String Int
+
+getDictIndex :: Directory -> String -> Int
+getDictIndex dict str = dict Map.! str
+
+putInDict :: Directory -> String -> Directory
+putInDict dict str = Map.insert str (Map.size dict) dict
+
+dictContains :: Directory -> String -> Bool
+dictContains dict str = Map.member str dict
+
+initDict :: Directory
+initDict = Map.fromList [(x:[], ord x) | x <- ['\0'..'\255']]
