75e87e0e7c1c92a9633fc61b6296d1c2e254f3db
Jordan Geurtsen <j.geurtsen1@student.han.nl>
2023-09-28 11:43:37 +0200

    Shortened naming, because it is already in the Dictionary.hs module. So naming like initDict can be shortened to init

--- Dictionary.hs
+++ Dictionary.hs
@@ -5,14 +5,14 @@ import System.Directory
 
 type Directory = Map.Map String Int
 
-getDictIndex :: Directory -> String -> Int
-getDictIndex dict str = dict Map.! str
+index :: Directory -> String -> Int
+index dict str = dict Map.! str
 
-putInDict :: Directory -> String -> Directory
-putInDict dict str = Map.insert str (Map.size dict) dict
+put :: Directory -> String -> Directory
+put dict str = Map.insert str (Map.size dict) dict
 
-dictContains :: Directory -> String -> Bool
-dictContains dict str = Map.member str dict
+contains :: Directory -> String -> Bool
+contains dict str = Map.member str dict
 
-initDict :: Directory
-initDict = Map.fromList [(x:[], ord x) | x <- ['\0'..'\255']]
+init :: Directory
+init = Map.fromList [(x:[], ord x) | x <- ['\0'..'\255']]
